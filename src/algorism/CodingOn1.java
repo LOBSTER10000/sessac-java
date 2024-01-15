0da8d41fe06d2cdad54a3a69c4c8cf19f9a8758c
LOBSTER10000 <111476138+LOBSTER10000@users.noreply.github.com>
2024-01-15 23:51:13 +0900

    33

--- src/algorism/CodingOn1.java
+++ src/algorism/CodingOn1.java
@@ -29,7 +29,7 @@ public class CodingOn1 {
         int result = 0;
         for(int i =0; i < nums.length; i++){
             if(nums[i] == target){
-                result = i;
+                result = i ;
             }
         }
         return result;
